42ddd0f4c1f8182e19a89da37c8f3d98f0ded09c
Sahil Khan <95959159+sahilkhan786000@users.noreply.github.com>
2023-09-12 12:28:24 +0530

    Update Cart.js

--- Client/src/screens/Cart.js
+++ Client/src/screens/Cart.js
@@ -19,7 +19,7 @@ export default function Cart() {
   const handleCheckOut = async () => {
     let userEmail = localStorage.getItem("userEmail");
     // console.log(data,localStorage.getItem("userEmail"),new Date())
-    let response = await fetch("https://food-api.onrender.com/api/orderData", {
+    let response = await fetch("https://food-api-uu9k.onrender.com/api/orderData", {
       // credentials: 'include',
       // Origin:"http://localhost:3000/login",
       method: 'POST',
